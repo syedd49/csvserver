25ae6461420fe92aafc114de667d81a47237b0e3
syedawsdevops49 <syedawsdevops49@gmail.com>
2023-08-25 00:23:48 +0530

    added script gencsv.sh

---
+++ solution/gencsv.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+# Define the number of rows for the CSV file 
+rows=3 
+# Generate the CSV file echo "Generating CSV file..." 
+# Generate data rows
+
+for (( i=0; i<=$rows; i++ )) 
+do 
+    #number1=$i number2=$((RANDOM % 300)) row="$number1, $number2" 
+    echo $row >> inputFile.csv 
+done 
+echo "CSV file generated successfully!"
